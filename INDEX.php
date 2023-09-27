045d298eb24a688923a7e4cc6e5d561e1aaffac6
ThiagoEwer <thiagoewer@gmail.com>
2023-09-27 13:40:59 -0300

    27/09/2023

---
+++ INDEX.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My blog</title>
+        <link href="https://fonts.googleapis.com/css?family=Handlee" rel="stylesheet">
+    </head>
+    
+    <body>
+        <?php include 'TOPO.php'; ?>
+        <?php include 'MENU.php'; ?>
+        <?php include 'CONTEUDO.php'; ?>
+        <?php include 'FALECONOSCO.php'; ?>
+        <?php include 'RODAPE.php'; ?>
+    </body>
+</html>
